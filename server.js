89a5e1cf1d047a346719e59022505907287bca25
sourav751998 <36950499+sourav751998@users.noreply.github.com>
2018-03-03 13:08:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.send('Article one request');
+});
+app.get('/article-two',function(req,res){
+    res.send('Article two request');
+});
+app.get('/article-three',function(req,res){
+    res.send('Article three request');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
