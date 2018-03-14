106f3dba797e04ed4033b8497d83b2444854f894
sourav751998 <36950499+sourav751998@users.noreply.github.com>
2018-03-14 19:42:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 alert('hii');
 var element =document.getElementById('main-txt');
 element.innerHTML='new value';
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
